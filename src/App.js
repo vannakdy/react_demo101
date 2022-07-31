eb0c5cecf23a6dcedfb71807d3b83738c2543334
vannakdy <dyvannak1997@gmail.com>
2022-07-31 21:20:56 +0700

    Commit first react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () =>{
+
+  return (
+    <div style={{padding:20}}>
+      <h1>Welcome ReactJS</h1>
+      <h1>Welcome ReactJS</h1>
+      <h1>Welcome ReactJS</h1>
+      <h1>Welcome ReactJS</h1>
+      <h1>Welcome ReactJS</h1>
+      <h1>Welcome ReactJS</h1>
+    </div>
+  )
+}
+
+export default App;
